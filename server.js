d674950ca4cfb877e039369f5d4333ad2b46b512
Luab <luabul@local-2.c.smart-display-177011.internal>
2017-10-10 14:50:05 +0000

    last stupid mistake

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require("express");
 express()
  .get("*", function(req, res) {
 	res.send("CI cnd CD lab finished (hopefully)"); 
-   });
+   
  })
  .listen(80, function(err) {
     if (err) {
